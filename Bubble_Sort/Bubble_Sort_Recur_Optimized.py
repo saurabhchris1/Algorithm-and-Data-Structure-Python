b78c4e3a6dbe77e2781bec2e4dfee77c6bc28d45
saurabhchris1 <saurabhchris1@gmail.com>
2019-10-22 14:27:06 -0700

    change implementation

--- Bubble_Sort/Bubble_Sort_Recur_Optimized.py
+++ Bubble_Sort/Bubble_Sort_Recur_Optimized.py
@@ -2,17 +2,21 @@
 # Input array and size of the array
 
 
-def bubble_sort_recursive(num_arr, size):
+def bubble_sort_recursive(num_arr, is_sorted, size):
     len_arr = len(num_arr)
+    is_swapped = is_sorted
 
-    if size >= 0:
+    if size >= 0 and is_sorted == False:
         for i in range(len_arr - 1):
 
             if num_arr[i + 1] < num_arr[i]:
                 num_arr[i], num_arr[i + 1] = num_arr[i + 1], num_arr[i]
+                is_swapped = True
 
-        return bubble_sort_recursive(num_arr, size - 1)
-
+        if is_swapped:
+            return bubble_sort_recursive(num_arr, False, size - 1)
+        else:
+            return num_arr
     else:
         return num_arr
 
@@ -20,7 +24,7 @@ def bubble_sort_recursive(num_arr, size):
 if __name__ == '__main__':
     num = [2, 11, 6, 4, 7, 8]
 
-    sorted_arr = bubble_sort_recursive(num, 5)
+    sorted_arr = bubble_sort_recursive(num, False, 5)
 
     print ("The sorted array is : " + str(sorted_arr))
 
