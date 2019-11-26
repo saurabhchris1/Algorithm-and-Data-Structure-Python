4caa1a13ca8868290f0b5a10bf0fa08c35927a6c
Saurabh Jaiswal <saurabhchris1@gmail.com>
2019-11-26 10:24:05 -0800

    Depth First Search iterative

--- Depth_First_Search/Depth_First_Search.py
+++ Depth_First_Search/Depth_First_Search.py
@@ -3,24 +3,30 @@
 class Node:
 
     def __init__(self, data=None, children=None):
-
         self.data = data
         self.children = children
 
     def dfs(self):
-
         self._dfshelper(self)
 
     def _dfshelper(self, node):
-
         print (node.data)
-
         if node.children != None:
             for child in node.children:
                 self._dfshelper(child)
 
+    def dfs_iterative(self):
+        stack = []
+        stack.append(self)
+        while len(stack) > 0:
+            node = stack.pop()
+            print (node.data)
+            if node.children != None:
+                stack += reversed(node.children)
+
 
 if __name__ == "__main__":
+
     a = Node('a')
     b = Node('b')
     c = Node('c')
@@ -30,3 +36,4 @@ if __name__ == "__main__":
     a.children = [b, c, d]
     b.children = [e, f]
     a.dfs()
+    a.dfs_iterative()
